8ab52f26b0c6adbc02c7f66e03a77d0f2031b8a1
Shaun Dowling <shaun.dowling@gmail.com>
2015-02-17 22:17:45 +0000

    added test / implementation for generating log format strings as json

---
+++ test/log-command.js
@@ -0,0 +1,19 @@
+var chai = require('chai')
+var assert = chai.assert
+
+var logcommand = require('../lib/log-command.js')
+
+describe("logcommand", function () {
+  it("should return a string", function () {
+    assert.isString(logcommand({}))
+  })
+
+  it("should return the command string for the fields specified", function () {
+    var actualCommand = logcommand({
+      fields: ["commitHash", "committerName", "committerDateUnix"]
+    })
+    assert.include(actualCommand, "commitHash")
+    assert.include(actualCommand, "committerName")
+    assert.include(actualCommand, "committerDateUnix")
+  })
+})
