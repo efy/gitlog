acd750db23689ae060ac089759e4ea7959ad5c73
Shaun Dowling <shaun.dowling@gmail.com>
2015-02-18 15:00:37 +0000

    Refactoring

--- test/log-parse.js
+++ test/log-parse.js
@@ -1,19 +1,19 @@
 var chai = require('chai')
 var assert = chai.assert
 
-var gitlog = require('../lib/log')
+var logexec = require('../lib/log-exec')
 var logparse = require('../lib/log-parse')
 
 describe('logparse', function () {
   it('should parse a raw log into an array of commit objects', function () {
-    gitlog(__dirname, function (error, log) {
+    logexec(__dirname, function (error, log) {
       assert.isArray(logparse(log))
       assert.isDefined(logparse(log)[0].commitHash)
     })
   })
 
   it('should parse dates into date objects', function () {
-    gitlog(__dirname, function (error, log) {
+    logexec(__dirname, function (error, log) {
       var commits = logparse(log)
 
       var lastCommitDate = commits[0].committerDate
